28391b69f34b76a34a6970ddedb55947ddc6b533
maximombro <maximombro@gmail.com>
2017-09-06 23:04:53 -0400

    Main menu complete, working on connecting media

--- mmcMainControl.py
+++ mmcMainControl.py
@@ -5,6 +5,7 @@ import os
 
 # Globals
 VERSION = "1.0"
+G_MEDIA = None
 
 # Main Thread
 def main():
@@ -12,10 +13,10 @@ def main():
     showWelcomeMessage()
 
     # Get Attached Media
-    media = getAttachedMedia()
+    G_MEDIA, nameList = getAttachedMedia()
 
     # Print Main Menu
-
+    showMainMenu(G_MEDIA,nameList)
 
 # Get included media files
 # Types:
@@ -23,30 +24,112 @@ def main():
 # Movie = NAME.*
 # Picture = NAME.*
 def getAttachedMedia():
-    # Initialize
+    # Intialize
     links = []
     movies = []
-    photos = []
+    #photos = []
+
+    # Populate Containers
+    try:
+        links = os.listdir("Media/Links")
+    except:
+        links.append("No Directory")
+
+    try:
+        movies = os.listdir("Media/Movies")
+    except:
+        movies.append("No Directory")
+
+    # try:
+    #     photos = os.listdir("Media/Pictures")
+    # except:
+    #     photos.append("No Directory")
 
     # Compile
-    mediaContainer = [links,movies,photos]
+    mediaContainer = [links,movies]
+    nameList = ["Media Links", "Movies"]
 
     # Return List
-    return mediaContainer
+    return mediaContainer, nameList
 
 # Print Welcome Message
 def showWelcomeMessage():
     message = "[ Welcome to T-Papi Multi-Media Controller v"+VERSION+" ]" 
-    print("-"*len(message))
     print(message)
     print("-"*len(message))
 
+# Print Title Message
+def showHeaderMessage(message):
+    header = "[ "+message+" ]"
+    print(header)
+    print("-"*len(header))
+
 # Print Main Menu Message
-def showMainMenu(media):
-    if len(media) != 0:
-        print("Place holder for media.")
+def showMainMenu(media,names):
+    # Variables
+    choiceLabel = 0
+    longestSpace = 0
+
+    # Get Spacing
+    tick = 0
+    for item in media:
+        if(longestSpace < len(names[tick]+" ")):
+            longestSpace = len(names[tick]+" ")
+        tick += 1
+
+    # Print Items
+    for item in media:
+        print("[ "+str(choiceLabel)+" ] "+names[choiceLabel], end='')
+        print(" "*(longestSpace-len(names[choiceLabel])), end='')
+        print("("+str(len(item))+" items)")
+        choiceLabel += 1
+
+    # Print Exit
+    print("[ "+str(choiceLabel)+" ] Exit Program")
+
+    # Get Valid Input
+    # Print Instructions
+    selection = input("Choose your option: ")
+
+    # Check Input
+    # TODO: Make recursive if invalid input
+    if selection == str(choiceLabel):
+        # Quit (it's always last)
+        quit()
     else:
-        print("No media to show.")
+        # Try to use input
+        try:
+            # Check if valid
+            test = media[int(selection)]
+
+            # If it gets here, it's valid
+            # Run Method Call
+            showContentMenu(names[int(selection)],media[int(selection)])
+        except:
+            print("Invalid input.")
+
+# Shows a new content menu
+def showContentMenu(message,items):
+    # Print Spacer
+    print("")
+
+    # Print Header
+    showHeaderMessage(message)
+
+    # Variables
+    choiceLabel = 0
+
+    # Print Back Option
+    print("[ "+str(choiceLabel)+" ] Go Back")
+    choiceLabel += 1
+
+    # Print Items
+    for item in items:
+        print("[ "+str(choiceLabel)+" ] "+item.rsplit(".", 1 )[0])
+        choiceLabel += 1
+
+    # Get Input
+    input("KILL")
 
 # Execute Main Thread
 main()
